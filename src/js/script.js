b0d9185399aa3269d71c1f2c42ffb70fc4f1f847
Igor Augusto <igoraugustobz@gmail.com>
2023-11-06 10:11:36 -0300

    perf: otimizações feitas

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,18 @@
 const html = document.documentElement; // Acessa o elemento <html>
 const switchButton = document.getElementById('switch');
 
+// Arrow Function que realiza a troca da imagem
 const toggleImage = () => {
-  const image = document.getElementById('image');
+  const image = document.querySelector('#profile img');
 
   if (html.classList.contains('light')) {
     image.src = 'assets/avatar-light.png';
+    image.alt =
+      'Foto de Igor Augusto sorrindo e camisa vermelha, cavanhaque e fundo gradiente (do azul para o roxo).';
   } else {
     image.src = 'assets/avatar.png';
+    image.alt =
+      'Foto de Igor Augusto sorrindo e camisa vermelha, cavanhaque e fundo laranja.';
   }
 };
 
